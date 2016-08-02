f5e966c4a52c8891e1639e3fc5c979429a7c8fb7
michael shamah <shamah.michael@gmail.com>
2016-08-02 12:01:49 -0400

    final tweeks

--- universities/app/assets/javascripts/main.js
+++ universities/app/assets/javascripts/main.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
 
   function createList(univs){
     $container= $('tbody')
+    $container.empty()
     univs.forEach((univ, index)=>{
       let row= $('<tr>')
       let $number= $('<td>').text(index)
